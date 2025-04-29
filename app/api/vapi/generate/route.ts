38abcb5d27868a2eb0c2bcb6e52e330cf59e86ba
rohya-28 <rohitarsude28@gmail.com>
2025-04-29 13:11:21 +0530

    check 1 solve

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -5,7 +5,7 @@ import { db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
-  const { type, role, level, techstack, amount, userId } = await request.json();
+  const { type, role, level, techstack, amount, userid } = await request.json();
 
   try {
     const { text: questions } = await generateText({
@@ -31,7 +31,7 @@ export async function POST(request: Request) {
       level: level,
       techstack: techstack.split(","),
       questions: JSON.parse(questions),
-      userId: userId,
+      userId: userid,
       finalized: true,
       coverImage: getRandomInterviewCover(),
       createdAt: new Date().toISOString(),
